36033c41fb86f67427e2b98aff4d2a6397189a6d
destro967 <destro967@users.noreply.github.com>
2019-11-01 01:15:34 +0200

    last try to fix. result: 1)tables are created 2)info inserts in tables 3)breaks after inserting question: 1)why???

--- src/main/java/ua/destro967/springboot/jpa/Application.java
+++ src/main/java/ua/destro967/springboot/jpa/Application.java
@@ -1,7 +1,7 @@
 package ua.destro967.springboot.jpa;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,15 +11,22 @@ import ua.destro967.springboot.jpa.Book.BookRepository;
 @Slf4j
 public class Application {
 
-    org.slf4j.Logger  slf4jLog = org.slf4j.LoggerFactory.getLogger("test");
-
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
 
     @Bean
-    public CommandLineRunner runner(BookRepository repository){
-        return r -> slf4jLog.info(repository.findAll().toString());
+    public String runner(BookRepository repository){
+        return repository.findAll().toString();
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        Flyway fly = new Flyway();
+        fly.setDataSource("jdbc:postgresql:books_test","test","test");
+        fly.setLocations("filesystem:src/main/resources/db/migration");
+
+        fly.migrate();
+        return fly;
     }
 }
