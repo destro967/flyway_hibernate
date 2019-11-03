1b5fff148f7efcd56b7ad02008ec5a42b8982d8a
destro967 <destro967@users.noreply.github.com>
2019-11-03 09:54:09 +0200

    maybe its better but i am not sure

---
+++ src/main/java/Customer.java
@@ -0,0 +1,46 @@
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="customer")
+public class Customer implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name="lastname")
+    private String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
