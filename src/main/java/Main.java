6b640ea5b7fe47a2015ce284a7f4b9ab012ab041
destro967 <destro967@users.noreply.github.com>
2019-11-03 10:33:30 +0200

    maybe its better but i am not sure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
+    }
+
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway(){
+        Flyway flyway = new Flyway();
+        flyway.setDataSource("jdbc:postgresql:books_test","test","test");
+        flyway.setLocations("filesystem:src/main/resources/db/migration");
+
+        flyway.migrate();
+        return flyway;
     }
 }
